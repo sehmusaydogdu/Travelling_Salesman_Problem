84e2322f670e70e3c4418cbc328692c6c4d8b3a8
sehmusaydogdu <sehmusaydogdu47@gmail.com>
2018-03-21 23:56:57 +0300

    update

--- TravellingSalesman/app/src/main/java/com/project/travellingsalesman/ButtonSetter.java
+++ TravellingSalesman/app/src/main/java/com/project/travellingsalesman/ButtonSetter.java
@@ -14,31 +14,39 @@ class ButtonSetter {
 
 
     ButtonSetter(int i,ScreenView screenView) {
+        point=screenView.getPoint();
         count=0;
         if(i==0) levelStateSetter(screenView);
         if(i==1) imageButtonSetter(screenView);
     }
 
+    private void SettingImage(float density){
+        if (density >= 4.0) {
+            //return "xxxhdpi";
+        }
+        if (density >= 3.0) {
+            //return "xxhdpi";
+        }
+        if (density >= 2.0) {//"xhdpi";
+            constX=30;constY=30; setWidth(200);setHeight(200);setX(constX);setY(constY);
+        }
+        if (density >= 1.5) {
+            //return "hdpi";
+        }
+        if (density >= 1.0) {
+           // return "mdpi";
+        }
+        //return "ldpi";
+    }
+
     private void imageButtonSetter(ScreenView screenView) {
-
-        point=screenView.getPoint();
-
-        constX=30;
-        constY=30;
-
-
-        setWidth(200);
-        setHeight(200);
-
-        setX(constX);
-        setY(constY);
+        SettingImage(screenView.getDensity());
     }
 
 
     private void levelStateSetter(ScreenView screenView){
         constX=50;
         constY=50;
-        point=screenView.getPoint();
 
         setWidth(200);
         setHeight(200);
